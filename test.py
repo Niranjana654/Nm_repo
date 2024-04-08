e748c821750050b1f6e1918eed1195efdd95fbc0
Niranjana654 <139219054+Niranjana654@users.noreply.github.com>
2024-04-08 19:01:23 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from PCA_approach import discriminator
-
-
-
-# if seed=0, then no seed is used
-seed = 1023112390
-discriminator.gui_init(save_path="PCA_approach/inference_saves/0.00341766_g6.78450571.pth", seed=seed)
